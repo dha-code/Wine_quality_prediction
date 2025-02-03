d27fb718c2408367d9e5c8ef2a613f034cee327c
dha-code <dhanya.sunilgkn@gmail.com>
2025-02-03 13:09:30 +0100

    Add option to generate synthetic data

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from utils.model import run_nn
 from utils.eda import process_input
+from utils.synthetic_data import generate_synthetic_data
+import kaggle
 
-input_filename = "./data/WineQT.csv"
-output_filename = "./data/WinesCleaned.csv"
+kaggle.api.dataset_download_file('yasserh/wine-quality-dataset','WineQT.csv', path='./data')
+wine_dataset = "./data/WineQT.csv"
+input_to_nn = "./data/WinesCleaned.csv"
+synthetic=False
 
-process_input(input_filename)
-run_nn(output_filename)
+if synthetic:
+    num_of_new_samples = 1000
+    synthetic_dataset = "./data/TVsynthetic_wine_data.csv"
+    generate_synthetic_data(input_to_nn, num_of_new_samples, synthetic_dataset)
+    input_to_nn = synthetic_dataset
+
+process_input(wine_dataset)
+run_nn(input_to_nn)
---
+++ utils/synthetic_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sdv.metadata import Metadata
+from sdv.single_table import TVAESynthesizer
+from sdv.evaluation.single_table import run_diagnostic, evaluate_quality
+from sdv.evaluation.single_table import get_column_plot
+
+def check_qual(df, synthetic_data, metadata):
+    # Function to check quality of the synthetic dataset
+    # The scores are printed on the std output and the image is saved to the figures folder
+    diagnostic = run_diagnostic(df, synthetic_data, metadata)
+    quality_report = evaluate_quality(df, synthetic_data, metadata)
+
+    fig = get_column_plot(
+        real_data=df,
+        synthetic_data=synthetic_data,
+        metadata=metadata,
+        column_name='quality'
+    )    
+    fig.write_image("./figures/Synthetic data - Quality dist.png")
+
+def generate_synthetic_data(filename, num_samples, outfile):
+    # Function to generate synthetic data given the original dataset
+    df = pd.read_csv(filename)
+    metadata = Metadata.detect_from_dataframe(
+        data=df,
+        table_name='Wines')
+    
+    model = TVAESynthesizer(metadata) 
+    model.fit(df)
+    synthetic_data = model.sample(num_samples)
+
+    expanded_df = pd.concat([df,synthetic_data])
+    expanded_df.to_csv(outfile, index=False)
+    check_qual(df, synthetic_data, metadata)
+
+# Main function
+if __name__ == "__main__":
+    generate_synthetic_data("./data/WinesCleaned.csv",1000, "./data/TVsynthetic_wine_data.csv")
